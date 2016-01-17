5b99dd4b9de49bff657d03e24e9cabe5cc7eae8b
Vladislav Danilchik <danilchican@mail.ru>
2016-01-17 23:59:24 +0300

    added migration for company table

---
+++ www/database/migrations/2016_01_17_184148_create_company_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('name', 255);
+            $table->text('description');
+            $table->string('logo_url', 255);
+            $table->integer('status')->default(0);  // 2 - approved, -1 - refused, 1 - expects moderation, 0 - none
+            $table->timestamp('created_at');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('companies');
+    }
+}
