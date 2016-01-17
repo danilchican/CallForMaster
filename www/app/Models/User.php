b0ca988fc6e31b11910333c36ebbb22b6f4a0871
Vladislav Danilchik <danilchican@mail.ru>
2016-01-17 22:43:58 +0300

    User model was moved to new folder called Models and was created company table in Database

--- www/app/Models/User.php
+++ www/app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -11,6 +11,7 @@ class User extends Authenticatable
      *
      * @var array
      */
+
     protected $fillable = [
         'email', 'password',
     ];
