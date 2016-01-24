e91550cd236b286b5f3c8fa0e38c712041d1a709
Vladislav Danilchik <danilchican@mail.ru>
2016-01-24 18:40:12 +0300

    Created partials and edited layouts files

---
+++ www/resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- SiteScripts -->
+    @yield('scripts')
+</body>
+</html>
