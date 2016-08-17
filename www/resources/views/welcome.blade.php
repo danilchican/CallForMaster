4cc26c1943be357f732fef7d737389e261902c3c
Vladislav Danilchik <danilchican@mail.ru>
2016-08-17 13:22:23 +0300

    Restructured view of index page

--- www/resources/views/welcome.blade.php
+++ www/resources/views/welcome.blade.php
@@ -26,28 +26,6 @@
             @endforeach
         </div><!-- /.row -->
 
-        @foreach($subHeadsCategories->take(2) as $category)
-            <div class="page-header">
-                <h3>{{ $category->name }}</h3>
-            </div>
-            @foreach ($category->children->take(4) as $child)
-                <div class="col-md-3 col-sm-3 col-xs-6 main-catalog-box">
-                    <img class="img-circle" data-src="holder.js/140x140" alt="140x140" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNDAiIGhlaWdodD0iMTQwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjcwIiB5PSI3MCIgc3R5bGU9ImZpbGw6I2FhYTtmb250LXdlaWdodDpib2xkO2ZvbnQtc2l6ZToxMnB4O2ZvbnQtZmFtaWx5OkFyaWFsLEhlbHZldGljYSxzYW5zLXNlcmlmO2RvbWluYW50LWJhc2VsaW5lOmNlbnRyYWwiPjE0MHgxNDA8L3RleHQ+PC9zdmc+" style="width: 140px; height: 140px;">
-                    <h4><a href="/categories/{{ $child->slug }}">{{ $child->name }}</a></h4>
-                </div><!-- /.col-lg-4 -->
-            @endforeach
-
-        @endforeach
     </div><!-- /.main-catalog -->
-
-    <div class="container" style="margin-bottom:20px;">
-        <div class="col-lg-12">
-            <div class="input-group">
-                <input type="text" class="form-control">
-                  <span class="input-group-btn">
-                      <button type="submit" class="btn btn-default">Найти</button>
-                  </span>
-            </div><!-- /input-group -->
-        </div><!-- /.col-lg-6 -->
-    </div><!-- /.container -->
+пше
 @endsection
