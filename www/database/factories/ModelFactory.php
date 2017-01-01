df8423a40dd491b21c4e628f55593f328f643a5d
danilchican <danilchican@mail.ru>
2017-01-01 18:26:31 +0300

    Created new seeder for tariffs table

--- www/database/factories/ModelFactory.php
+++ www/database/factories/ModelFactory.php
@@ -11,11 +11,21 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Models\Tariff::class, function (Faker\Generator $faker) {
+    return [
+        'title'              => $faker->realText(rand(10,15)),
+        'whom'               => $faker->realText(rand(20,40)),
+        'additional_service' => $faker->realText(rand(20,50)),
+        'top'                => $faker->numberBetween(1, 10),
+        'published'          => $faker->numberBetween(0, 1),
     ];
 });
