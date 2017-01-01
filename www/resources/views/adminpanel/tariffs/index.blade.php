ba05829ba510bb04ef2b75c6c80e2ff37d7ab43c
danilchican <danilchican@mail.ru>
2017-01-01 18:50:43 +0300

    Modified index view for tariffs #2

--- www/resources/views/adminpanel/tariffs/index.blade.php
+++ www/resources/views/adminpanel/tariffs/index.blade.php
@@ -27,57 +27,75 @@
             any tariff by clicking on buttons.</p>" !!}
         </div>
 
+        <div class="col-xs-12" style="margin-bottom: 15px;">
+            <div class="row">
+                <div class="col-xs-2">
+                    <div class="row">
+                        <a href="{{ route('admin.tariffs.create') }}">
+                            <button type="button" class="btn btn-block btn-primary">New tariff</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="row">
             <div class="col-xs-12">
                 <div class="box no-margin">
                     <div class="box-header">
                         <h3 class="box-title">{{ $title }}</h3>
-                        <div class="box-tools">
-                            <div class="input-group" style="width: 150px;">
-                                <input type="text" name="table_search" class="form-control input-sm pull-right" placeholder="Search">
-                                <div class="input-group-btn">
-                                    <button class="btn btn-sm btn-default"><i class="fa fa-search"></i></button>
+                        @if(count($tariffs) > 0)
+                            <div class="box-tools">
+                                <div class="input-group" style="width: 150px;">
+                                    <input type="text" name="table_search" class="form-control input-sm pull-right" placeholder="Search">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-sm btn-default"><i class="fa fa-search"></i></button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+                        @endif
                     </div><!-- /.box-header -->
-                    <div class="box-body table-responsive no-padding no-margin">
-                        <table class="table table-bordered table-striped table-hover">
-                            <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Title</th>
-                                <th>Top</th>
-                                <th>Created at</th>
-                                <th>Published</th>
-                                <th>Action</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($tariffs as $tariff)
+                        <div class="box-body table-responsive @if(count($tariffs) > 0) no-padding no-margin @endif">
+                            @if(count($tariffs) > 0)
+                            <table class="table table-bordered table-striped table-hover">
+                                <thead>
                                 <tr>
-                                    <td>{{ $tariff->id }}.</td>
-                                    <td><a href="/adminpanel/tariffs/view/{{ $tariff->id }}" data-toggle="tooltip" data-original-title="View Tariff">{{ $tariff->title }}</a></td>
-                                    <td>TOP {{ $tariff->top }}</td>
-                                    <td>{{ $tariff->created_at->format('m.d.Y H:i') }}</td>
-                                    <td>
-                                        @if($tariff->published)
-                                            <span class="label label-success">Published</span>
-                                        @else
-                                            <span class="label label-danger">Not published</span>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <div class="btn-group">
-                                            <a href="/adminpanel/tariffs/edit/{{ $tariff->id }}"><button type="button" class="btn btn-info btn-xs">Edit</button></a>
-                                            <a href="/adminpanel/tariffs/delete/{{ $tariff->id }}"><button type="button" class="btn btn-danger btn-xs">Delete</button></a>
-                                        </div>
-                                    </td>
+                                    <th>ID</th>
+                                    <th>Title</th>
+                                    <th>Top</th>
+                                    <th>Created at</th>
+                                    <th>Published</th>
+                                    <th>Action</th>
                                 </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div><!-- /.box-body -->
+                                </thead>
+                                <tbody>
+                                @foreach($tariffs as $tariff)
+                                    <tr>
+                                        <td>{{ $tariff->id }}.</td>
+                                        <td><a href="/adminpanel/tariffs/view/{{ $tariff->id }}" data-toggle="tooltip" data-original-title="View Tariff">{{ $tariff->title }}</a></td>
+                                        <td>TOP {{ $tariff->top }}</td>
+                                        <td>{{ $tariff->created_at->format('m.d.Y H:i') }}</td>
+                                        <td>
+                                            @if($tariff->published)
+                                                <span class="label label-success">Published</span>
+                                            @else
+                                                <span class="label label-danger">Not published</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <div class="btn-group">
+                                                <a href="/adminpanel/tariffs/edit/{{ $tariff->id }}"><button type="button" class="btn btn-info btn-xs">Edit</button></a>
+                                                <a href="/adminpanel/tariffs/delete/{{ $tariff->id }}"><button type="button" class="btn btn-danger btn-xs">Delete</button></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                            @else
+                                <h4>Тарифов пока нет.</h4>
+                            @endif
+                        </div><!-- /.box-body -->
                 </div><!-- /.box -->
             </div><!-- /.col -->
         </div><!-- /.row -->
